ea6e49daeb3a53be81b25a3b29582612592312d8
Pathindu <desilva-cs22066@stu.kln.ac.lk>
2025-09-10 19:22:59 +0530

    Check rules

--- LabWorksheets/src/LW_02/Q_1.java
+++ LabWorksheets/src/LW_02/Q_1.java
@@ -4,6 +4,7 @@ public class Q_1 {
     public static void main(String[] args) {
         for (int i = 10; i < 50; i++) {
             if (i % 10 == 0) {
+//                Breake the line
                 System.out.print("\n");
             }
             System.out.printf("%d\t", i);
