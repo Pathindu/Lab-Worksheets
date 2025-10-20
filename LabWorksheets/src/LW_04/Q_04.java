25449435024b2475dbb48a1fefc49ca3c87481da
Pathindu <desilva-cs22066@stu.kln.ac.lk>
2025-10-20 17:08:01 +0530

    LW_04_Q_10,11,12

--- LabWorksheets/src/LW_04/Q_04.java
+++ LabWorksheets/src/LW_04/Q_04.java
@@ -11,23 +11,21 @@ public class Q_04 {
 
         sc.close();
 
-        if (year % 4 != 0) {
-            System.out.println("It is not a leap year");
-        } else if (year % 100 != 0) {
-            System.out.println("It is a leap year");
-        } else if (year % 400 == 0) {
-            System.out.println("It is a leap year");
-        }else {
-            System.out.println("It is not a leap year");
-        }
-
-//
-//        if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 ==0)) {
+//        if (year % 4 != 0) {
+//            System.out.println("It is not a leap year");
+//        } else if (year % 100 != 0) {
 //            System.out.println("It is a leap year");
-//        } else {
+//        } else if (year % 400 == 0) {
+//            System.out.println("It is a leap year");
+//        }else {
 //            System.out.println("It is not a leap year");
 //        }
-//
 
+
+        if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 ==0)) {
+            System.out.println("It is a leap year");
+        } else {
+            System.out.println("It is not a leap year");
+        }
     }
 }
