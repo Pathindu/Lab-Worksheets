23b8e4ad1bddd1412547aabe0a19daf179043266
Pathindu <desilva-cs22066@stu.kln.ac.lk>
2025-11-11 16:04:59 +0530

    LW_07, 08

---
+++ LabWorksheets/src/LW_07/Q_01.java
@@ -0,0 +1,7 @@
+package LW_07;
+
+public class Q_01 {
+    public static void main(String[] args) {
+        new RegistrationForm();
+    }
+}
---
+++ LabWorksheets/src/LW_08/Q_01.java
@@ -0,0 +1,7 @@
+package LW_08;
+
+public class Q_01 {
+    public static void main(String[] args) {
+        new SignUpPage();
+    }
+}
